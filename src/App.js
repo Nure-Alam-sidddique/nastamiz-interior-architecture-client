d14f12fcdcee5656135533de42fe60ea8fd2984c
Nure Alam Siddique <nure.alam91@gmail.com>
2021-12-26 13:33:31 +0600

    firebase config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InitializationAuthentication from './Firebase/firebase.init';
+InitializationAuthentication();
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to our Nastamiz Interior House design!!!!!</h1>
+    </div>
+  );
+}
+
+export default App;
